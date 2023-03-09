50a441364722287a1abe23e445883fc80e1b4c6b
sanjay3937 <makwanasanjaylm@gmail.com>
2023-03-09 16:08:59 +0530

    sanjay new commit

--- resources/views/admin/User/index.blade.php
+++ resources/views/admin/User/index.blade.php
@@ -4,49 +4,8 @@
 User
 @endsection
 <header>
-    <style>
-
-        .serch{
-            box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-            border-radius: 10px;
-            margin: 15px;
-            padding: 10px;
-            
-        }
-        .button {
-            display: inline-block;
-            color: #ffffff;
-            background: linear-gradient(to right,
-                    #4c2fb2,
-                    #8524a2);
-            text-decoration: none;
-            padding: 10px 10px;
-            border: 3px solid #000000;
-            border-radius: 10px;
-            margin: 10px;
-
-        }
-        tr,td{
-            box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-           
-        }
-        
-
-        .button:hover {
-            animation: pulsate 5s ease-in-out;
-        }
-
-        @keyframes pulsate {
-            0% {
-                box-shadow:
-                    0 0 25px #3d04b8,
-                    0 0 50px #4407be;
-            }
-        }
-
-    </style>
+        <link rel="stylesheet" href="{{  URL::asset('css/index.css') }}">
 </header>
-
 
 @section('body')
 
@@ -59,7 +18,7 @@ User
     </div>
     @endif
     <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">User</li>
+        <li class="breadcrumb-item active" style="color:#000">User</li>
     </ol>
     @if(session('success'))
     <div class="alert">
@@ -67,11 +26,11 @@ User
     </div>
     @endif
     <a href="{{ route('user.create') }}">
-        <button type="button" class="button" class="btn rounded text-right btn-outline-warning">
+        <button type="button" class="button" class="btn rounded text-right btn-outline-warning  " >
             Add <i class="fa-solid fa-plus px-3"></i> </button>
     </a>
     <div class="card mb-4">
-       
+
 
         <div class="card-body" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;">
             <div class="mt-1 mb-4">
@@ -87,10 +46,10 @@ User
                 </div>
             </div>
 
-            <table class="table table-bordered" style= "box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;">
+            <table class="table table-bordered" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;">
                 <thead>
                     <tr>
-                        <th>First Name</th>
+                        <th> Name</th>
                         <th>Last Name</th>
                         <th>Email</th>
                         <th>Employee Id</th>
@@ -102,14 +61,14 @@ User
                 <tbody>
                     @foreach ($data as $d)
                     <tr>
-                        <td>{{ $d->first_name }}</td>
-                        <td>{{ $d->last_name }}</td>
-                        <td>{{ $d->email }}</td>
-                        <td>{{ $d->employee_id }}</td>
-                        <td>{{ $d->department }}</td>
+                        <td id="data">{{ $d->first_name }}</td>
+                        <td id="data">{{ $d->last_name }}</td>
+                        <td id="data">{{ $d->email }}</td>
+                        <td id="data">{{ $d->employee_id }}</td>
+                        <td id="data">{{ $d->department }}</td>
                         <td>
                             @if($d->status)
-                            <div class="h5 text-success">
+                            <div class="h5 text-success" id="data">
                                 Active
                             </div>
                             @else
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,4 +1,15 @@
-<div id="layoutSidenav_nav">
+<header>
+    <style>
+        .container>img {
+
+           filter: brightness(0) invert(1);
+
+        }
+       
+    </style>
+</header>
+
+<div id="layoutSidenav_nav" style=" width:15% " >
     <nav class="sb-sidenav accordion sb-sidenav zindex-sticky bg-orange" id="sidenavAccordion">
         <div class="sb-sidenav-menu align-content-center">
             <div class="nav">
@@ -7,7 +18,7 @@
 
                 <a class="nav-link" href="/user">
                     <div class='container'>
-                        <i class="fa-solid fa-user text-white"></i>
+                        <img src="images/use.png" style="width:17px" alt="">
                         <span class="text-white ">
                             User
                         </span>
@@ -17,7 +28,7 @@
                 <a class="nav-link" href="/cmspage">
                     <div class='container'>
                         <!-- {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}} -->
-                        <i class="fa-solid fa-pen-to-square text-white"></i>
+                        <img src="images/pag.png" style="width:17px" alt="">
                         <span class="text-white ">
                             CMS Page
                         </span>
@@ -25,15 +36,15 @@
                 </a>
                 <a class="nav-link" href="/mission">
                     <div class='container'>
-                        <i class="fa-solid fa-bullseye text-white"></i>
+                        <img src="images/tar.png" style="width:17px" alt="">
                         <span class="text-white ">
                             Mission
                         </span>
                     </div>
                 </a>
                 <a class="nav-link" href="/missiontheme">
-                    <div class='container-fluid'>
-                        <i class="fa-light fa-rectangles-mixed"></i>
+                    <div class='container'>
+                        <img src="images/missiontheme.png" alt="" style="width:17px">
                         <span class="text-white text-active">
                             Mission Theme
                         </span>
@@ -42,7 +53,7 @@
 
                 <a class="nav-link" href="/missionskill">
                     <div class='container'>
-                        <i class="fa-solid fa-folder text-white"></i>
+                        <img src="images/pen.png" style="width:17px" alt="">
                         <span class="text-white">
                             Mission skill
                         </span>
@@ -50,7 +61,7 @@
                 </a>
                 <a class="nav-link" href="#">
                     <div class='container'>
-                        <i class="fa-solid fa-folder text-white"></i>
+                       <img src="images/fol.png" style="width:17px" alt="">
                         <span class="text-white">
                             Mission Application
                         </span>
@@ -58,15 +69,15 @@
                 </a>
                 <a class="nav-link" href="#">
                     <div class='container'>
-                        <i class="fa-solid fa-book-blank text-white"></i>
+                        <img src="images/sr.png" alt=""  style="width:17px">
                         <span class="text-white ">
                             Story
                         </span>
                     </div>
                 </a>
                 <a class="nav-link" href="#">
-                    <div class='container-fluid'>
-                        <i class="fa-brands fa-canadian-maple-leaf text-white"></i>
+                    <div class='container'>
+                        <img src="images/ben.png" alt=""  style="width:17px">
                         <span class="text-white ">
                             Banner Management
                         </span>
