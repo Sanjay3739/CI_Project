e8b65cef5c74debca9db7953818b556ccd7f727d
aniketbhalani <72142200+aniketbhalani@users.noreply.github.com>
2023-03-10 11:44:18 +0530

    chsnges

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,25 +1,21 @@
-<header>
-    <style>
-        .container>img {
-
-           filter: brightness(0) invert(1);
-
-        }
-       
-    </style>
-</header>
-
-<div id="layoutSidenav_nav" style=" width:15% " >
+<div id="layoutSidenav_nav">
     <nav class="sb-sidenav accordion sb-sidenav zindex-sticky bg-orange" id="sidenavAccordion">
         <div class="sb-sidenav-menu align-content-center">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading text-white">NAVIGATION</div>
 
+                {{-- <a class="nav-link" href="index.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
 
-                <a class="nav-link" href="/user">
+                    <div class="card bg-light mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Header</div>
+                    </div>
+                </a> --}}
+                <a class="nav-link" href="{{route('user.index')}}">
                     <div class='container'>
-                        <img src="images/use.png" style="width:17px" alt="">
-                        <span class="text-white ">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-user text-white"></i>
+                        <span class="text-white font-weight-light">
                             User
                         </span>
                     </div>
@@ -27,24 +23,26 @@
 
                 <a class="nav-link" href="/cmspage">
                     <div class='container'>
-                        <!-- {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}} -->
-                        <img src="images/pag.png" style="width:17px" alt="">
-                        <span class="text-white ">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-pen-to-square text-white"></i>
+                        <span class="text-white font-weight-light ">
                             CMS Page
                         </span>
                     </div>
                 </a>
                 <a class="nav-link" href="/mission">
                     <div class='container'>
-                        <img src="images/tar.png" style="width:17px" alt="">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-bullseye text-white"></i>
                         <span class="text-white ">
                             Mission
                         </span>
                     </div>
                 </a>
                 <a class="nav-link" href="/missiontheme">
-                    <div class='container'>
-                        <img src="images/missiontheme.png" alt="" style="width:17px">
+                    <div class='container-fluid'>
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-folder text-white"></i>
                         <span class="text-white text-active">
                             Mission Theme
                         </span>
@@ -53,7 +51,8 @@
 
                 <a class="nav-link" href="/missionskill">
                     <div class='container'>
-                        <img src="images/pen.png" style="width:17px" alt="">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-folder text-white"></i>
                         <span class="text-white">
                             Mission skill
                         </span>
@@ -61,7 +60,8 @@
                 </a>
                 <a class="nav-link" href="#">
                     <div class='container'>
-                       <img src="images/fol.png" style="width:17px" alt="">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-solid fa-folder text-white"></i>
                         <span class="text-white">
                             Mission Application
                         </span>
@@ -69,22 +69,81 @@
                 </a>
                 <a class="nav-link" href="#">
                     <div class='container'>
-                        <img src="images/sr.png" alt=""  style="width:17px">
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-brands fa-canadian-maple-leaf text-white"></i>
                         <span class="text-white ">
                             Story
                         </span>
                     </div>
                 </a>
                 <a class="nav-link" href="#">
-                    <div class='container'>
-                        <img src="images/ben.png" alt=""  style="width:17px">
+                    <div class='container-fluid'>
+                        {{-- <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button> --}}
+                        <i class="fa-brands fa-canadian-maple-leaf text-white"></i>
                         <span class="text-white ">
                             Banner Management
                         </span>
                     </div>
                 </a>
 
+                {{-- <div class="sb-sidenav-menu-heading">Interface</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Layouts
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                    </nav>
+                </div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                    Pages
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                            Authentication
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="login.html">Login</a>
+                                <a class="nav-link" href="register.html">Register</a>
+                                <a class="nav-link" href="password.html">Forgot Password</a>
+                            </nav>
+                        </div>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                            Error
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="401.html">401 Page</a>
+                                <a class="nav-link" href="404.html">404 Page</a>
+                                <a class="nav-link" href="500.html">500 Page</a>
+                            </nav>
+                        </div>
+                    </nav>
+                </div>
+                <div class="sb-sidenav-menu-heading">Addons</div>
+                <a class="nav-link" href="charts.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Charts
+                </a>
+                <a class="nav-link" href="tables.html">
+                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                    Tables
+                </a>
             </div>
         </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logged in as:</div>
+            CI platform
+        </div> --}}
     </nav>
 </div>
+
