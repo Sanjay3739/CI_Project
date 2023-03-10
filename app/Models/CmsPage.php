c17309ff552e77bad3660785c83acf2d9c0136dd
aniketbhalani <72142200+aniketbhalani@users.noreply.github.com>
2023-03-10 11:26:28 +0530

    cms changes

--- app/Models/CmsPage.php
+++ app/Models/CmsPage.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CmsPage extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $primaryKey = 'cms_page_id';
+    protected $table = 'cms_pages';
+
+    protected $fillable = [
+        'cms_page_id',
+        'title',
+        'text',
+        'slug'
+    ];
+    
 }
